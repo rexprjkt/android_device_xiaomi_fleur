48462b4d5345d63d8788a107a5a893bf72b0a1a1
bengris32 <bengris32@protonmail.ch>
2024-10-09 17:55:44 +0000

    fleur: Patch ImsService.apk extract time
    * Also move ImsService to system_ext.
    
    Change-Id: I2ef962ddb5c738cc7682d2b5441929313ed1f321
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -208,7 +208,7 @@ PRODUCT_PACKAGES += \
     libui_shim
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
